909e7a30f35554cb0e8befd11b75c04c534fc031
Constantine <vasilevconst@gmail.com>
2016-11-20 01:29:38 +0300

    new class

---
+++ gitTestApp/NewClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gitTestApp
+{
+    class NewClass
+    {
+        public static void MyMethod(){
+            Console.WriteLine("ssssss");
+        }
+        
+    }
+}
